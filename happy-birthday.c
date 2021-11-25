b9256afa4bb1658ab52a95f09a184f0cf4b64c6c
taris14891859 <77280812+taris14891859@users.noreply.github.com>
2021-11-25 21:23:03 +0900

    Create happy-birthday.c
    first commit

---
+++ happy-birthday.c
@@ -0,0 +1,265 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(void){
+
+  const int SLTIME = 1000000;
+
+  char *pt1;
+  char *pt2;
+  char *pt3;
+  char *pt4;
+  char *pt5;
+  char *pt6;
+  char *pt7;
+
+  /* base text */
+  char *str1 = "\n";
+  char *str2 = "\n";
+  char *str3 = "\n";
+  char *str4 = "    ∩ ∩\n";
+  char *str5 = "  (｡･×･｡) PUI！\n";
+  char *str6 = "   ヽ っ＼   ／  \n";
+  char *str7 = "   ∪ ∪   / 🎀\n";
+
+  system("clear");   /* 画面をクリア */
+
+/* first frame */
+  pt1 = str1;
+  pt2 = str2;
+  pt3 = str3;
+  pt4 = str4;
+  pt5 = str5;
+  pt6 = str6;
+  pt7 = str7;
+
+  while(*pt1) {
+    printf("%c",*pt1);
+    pt1++;
+  }
+  while(*pt2) {
+    printf("%c",*pt2);
+    pt2++;
+  }
+  while(*pt3) {
+    printf("%c",*pt3);
+    pt3++;
+  }
+  while(*pt4) {
+    printf("%c",*pt4);
+    pt4++;
+  }
+  while(*pt5) {
+    printf("%c",*pt5);
+    pt5++;
+  }
+  while(*pt6) {
+    printf("%c",*pt6);
+    pt6++;
+  }
+  while(*pt7) {
+    printf("%c",*pt7);
+    pt7++;
+  }
+
+  usleep(SLTIME);
+  system("clear");   /* 画面をクリア */
+
+  /*second frame */
+  str1 = "\n";
+  str2 = "\n";
+  str3 = "\n";
+  str4 = "    ∩ ∩\n";
+  str5 = "  (｡･×･｡)っ🦯 PUI✨\n";
+  str6 = "   ヽ っ＼   ／  \n";
+  str7 = "   ∪ ∪   / 🎀🤍\n";
+
+  pt1 = str1;
+  pt2 = str2;
+  pt3 = str3;
+  pt4 = str4;
+  pt5 = str5;
+  pt6 = str6;
+  pt7 = str7;
+
+  while(*pt1) {
+    printf("%c",*pt1);
+    pt1++;
+  }
+  while(*pt2) {
+    printf("%c",*pt2);
+    pt2++;
+  }
+  while(*pt3) {
+    printf("%c",*pt3);
+    pt3++;
+  }
+  while(*pt4) {
+    printf("%c",*pt4);
+    pt4++;
+  }
+  while(*pt5) {
+    printf("%c",*pt5);
+    pt5++;
+  }
+  while(*pt6) {
+    printf("%c",*pt6);
+    pt6++;
+  }
+  while(*pt7) {
+    printf("%c",*pt7);
+    pt7++;
+  }
+
+  usleep(SLTIME/3);
+  system("clear");   /* 画面をクリア */
+
+  /*third frame */
+  str1 = "\n";
+  str2 = "\n";
+  str3 = "\n";
+  str4 = "    ∩ ∩\n";
+  str5 = "  (｡･×･｡)っ🦯    PUI✨\n";
+  str6 = "   ヽ っ＼   ／  \n";
+  str7 = "   ∪ ∪   / 🎀🤍\n";
+
+  pt1 = str1;
+  pt2 = str2;
+  pt3 = str3;
+  pt4 = str4;
+  pt5 = str5;
+  pt6 = str6;
+  pt7 = str7;
+
+  while(*pt1) {
+    printf("%c",*pt1);
+    pt1++;
+  }
+  while(*pt2) {
+    printf("%c",*pt2);
+    pt2++;
+  }
+  while(*pt3) {
+    printf("%c",*pt3);
+    pt3++;
+  }
+  while(*pt4) {
+    printf("%c",*pt4);
+    pt4++;
+  }
+  while(*pt5) {
+    printf("%c",*pt5);
+    pt5++;
+  }
+  while(*pt6) {
+    printf("%c",*pt6);
+    pt6++;
+  }
+  while(*pt7) {
+    printf("%c",*pt7);
+    pt7++;
+  }
+
+for(int i=0; i<=5; i++) {
+
+    usleep(SLTIME);
+    system("clear");   /* 画面をクリア */
+
+    /*Third frame */
+    str1 = "✨\n";
+    str2 = "     🤍🌹🌹🌹🌹🤍\n";
+    str3 = "   🤍🌹 Happy 🌹🤍\n";
+    str4 = "    ∩ 🌹 Birthday 🌹🤍\n";
+    str5 = "  (｡･×･🌹🌹🌹🌹🌹🤍\n";
+    str6 = "   ヽ っ＼   ／        ✨\n";
+    str7 = "   ∪ ∪   / 🎀🤍\n";
+
+    pt1 = str1;
+    pt2 = str2;
+    pt3 = str3;
+    pt4 = str4;
+    pt5 = str5;
+    pt6 = str6;
+    pt7 = str7;
+
+    while(*pt1) {
+      printf("%c",*pt1);
+      pt1++;
+    }
+    while(*pt2) {
+      printf("%c",*pt2);
+      pt2++;
+    }
+    while(*pt3) {
+      printf("%c",*pt3);
+      pt3++;
+    }
+    while(*pt4) {
+      printf("%c",*pt4);
+      pt4++;
+    }
+    while(*pt5) {
+      printf("%c",*pt5);
+      pt5++;
+    }
+    while(*pt6) {
+      printf("%c",*pt6);
+      pt6++;
+    }
+    while(*pt7) {
+      printf("%c",*pt7);
+      pt7++;
+    }
+
+    usleep(SLTIME);
+    system("clear");   /* 画面をクリア */
+
+    /*fourth frame */
+    str1 = "                      ✨\n";
+    str2 = "     🤍🌹🌹🌹🌹🤍\n";
+    str3 = "   🤍🌹 Alice 🌹🤍\n";
+    str4 = "    ∩ 🌹 Yumemiya 🌹🤍\n";
+    str5 = "✨(｡･×･🌹🌹🌹🌹🌹🤍\n";
+    str6 = "   ヽ っ＼   ／\n";
+    str7 = "   ∪ ∪   / 🎀🤍\n";
+
+    pt1 = str1;
+    pt2 = str2;
+    pt3 = str3;
+    pt4 = str4;
+    pt5 = str5;
+    pt6 = str6;
+    pt7 = str7;
+
+    while(*pt1) {
+      printf("%c",*pt1);
+      pt1++;
+    }
+    while(*pt2) {
+      printf("%c",*pt2);
+      pt2++;
+    }
+    while(*pt3) {
+      printf("%c",*pt3);
+      pt3++;
+    }
+    while(*pt4) {
+      printf("%c",*pt4);
+      pt4++;
+    }
+    while(*pt5) {
+      printf("%c",*pt5);
+      pt5++;
+    }
+    while(*pt6) {
+      printf("%c",*pt6);
+      pt6++;
+    }
+    while(*pt7) {
+      printf("%c",*pt7);
+      pt7++;
+    }
+  }
+
+}
